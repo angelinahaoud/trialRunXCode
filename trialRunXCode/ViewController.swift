326be4d1ee0c1ff9ac3e34b359b4de034928bd87
angelina <fannanangie@gmail.com>
2022-06-28 11:29:38 -0400

    Initial Commit

---
+++ trialRunXCode/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  trialRunXCode
+//
+//  Created by Angelina Haoud on 6/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
